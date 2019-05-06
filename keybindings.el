a683e6cfb6f01d2429a79c5d4ef6cec45e8818d4
Anmol Jagetia <anmol.j@media.net>
2019-05-07 03:20:55 +0530

    Adds keybindings to the layer

---
+++ keybindings.el
@@ -0,0 +1,11 @@
+;; Labels the app as Leetcode se it doesn't appear as "prefix" in the menu
+(spacemacs/declare-prefix "a L" "Leetcode")
+
+;; The remaining useful keybindings to using Leetcode
+(spacemacs/set-leader-keys
+  "a L l" 'leetcode
+  "a L d" 'leetcode-show-descri
+  "a L r" 'leetcode--problems-refresh
+  "a L t" 'leetcode-try
+  "a L u" 'leetcode-submit
+  )
